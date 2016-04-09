3c0c047fe3d545325e4edeb4a11cea966508b923
Matthias Kurz <m.kurz@irregular.at>
2016-04-10 00:52:53 +0200

    Removed deprecated JPA methods which allows us to get rid of...
    ...the ThreadLocal storing the entity manager. The only thing
    that's left is that we have to put the entity manager into the context.args

--- framework/src/play-java-jpa/src/main/java/play/db/jpa/JPAEntityManagerContext.java
+++ framework/src/play-java-jpa/src/main/java/play/db/jpa/JPAEntityManagerContext.java
@@ -6,31 +6,21 @@ import javax.persistence.EntityManager;
 import java.util.ArrayDeque;
 import java.util.Deque;
 
-public class JPAEntityManagerContext extends ThreadLocal<Deque<EntityManager>> {
+public class JPAEntityManagerContext {
 
     private static final String CURRENT_ENTITY_MANAGER = "entityManagerContext";
 
-    @Override
-    public Deque<EntityManager> initialValue() {
-        return new ArrayDeque<>();
-    }
-
     /**
-     * Get the default EntityManager for this thread.
+     * Get the default EntityManager for the current Http.Context.
      *
-     * @throws RuntimeException if no EntityManager is bound to the current Http.Context or the current Thread.
+     * @throws RuntimeException if no EntityManager is bound to the current Http.Context or the current Http.Context.
      * @return the EntityManager
      */
-    public EntityManager em() {
-        Http.Context context = Http.Context.current.get();
-        Deque<EntityManager> ems = this.emStack(true);
+    public static EntityManager em() {
+        Deque<EntityManager> ems = emStack();
 
         if (ems.isEmpty()) {
-            if (context != null) {
-                throw new RuntimeException("No EntityManager found in the context. Try to annotate your action method with @play.db.jpa.Transactional");
-            } else {
-                throw new RuntimeException("No EntityManager bound to this thread. Try wrapping this call in JPAApi.withTransaction, or ensure that the HTTP context is setup on this thread.");
-            }
+            throw new RuntimeException("No EntityManager found in the Http.Context. Try to annotate your action method with @play.db.jpa.Transactional");
         }
 
         return ems.peekFirst();
@@ -40,7 +30,7 @@ public class JPAEntityManagerContext extends ThreadLocal<Deque<EntityManager>> {
      * Get the EntityManager stack.
      */
     @SuppressWarnings("unchecked")
-    public Deque<EntityManager> emStack(boolean threadLocalFallback) {
+    public static Deque<EntityManager> emStack() {
         Http.Context context = Http.Context.current.get();
         if (context != null) {
             Object emsObject = context.args.get(CURRENT_ENTITY_MANAGER);
@@ -52,47 +42,22 @@ public class JPAEntityManagerContext extends ThreadLocal<Deque<EntityManager>> {
                 return ems;
             }
         } else {
-            // Not a web request
-            if (threadLocalFallback) {
-                return this.get();
-            } else {
-                throw new RuntimeException("No Http.Context is present. If you want to invoke this method outside of a HTTP request, you need to wrap the call with JPA.withTransaction instead.");
-            }
+            throw new RuntimeException("No Http.Context is present. If you want to invoke this method outside of a HTTP request, you need to wrap the call with JPA.withTransaction instead.");
         }
     }
 
-    public void push(EntityManager em, boolean threadLocalFallback) {
-        Deque<EntityManager> ems = this.emStack(threadLocalFallback);
+    public static void push(EntityManager em) {
+        Deque<EntityManager> ems = emStack();
         if (em != null) {
             ems.push(em);
         }
     }
 
-    public void pop(boolean threadLocalFallback) {
-        Deque<EntityManager> ems = this.emStack(threadLocalFallback);
+    public static void pop() {
+        Deque<EntityManager> ems = emStack();
         if (ems.isEmpty()) {
             throw new IllegalStateException("Tried to remove the EntityManager, but none was set.");
         }
         ems.pop();
-    }
-
-    /**
-     * Pushes or pops the EntityManager stack depending on the value of the
-     * em argument. If em is null, then the current EntityManager is popped. If em
-     * is non-null, then em is pushed onto the stack and becomes the current EntityManager.
-     *
-     * @deprecated use push or pop methods
-     */
-    @Deprecated
-    public void pushOrPopEm(EntityManager em, boolean threadLocalFallback) {
-        Deque<EntityManager> ems = this.emStack(threadLocalFallback);
-        if (em != null) {
-            ems.push(em);
-        } else {
-            if (ems.isEmpty()) {
-                throw new IllegalStateException("Tried to remove the EntityManager, but none was set.");
-            }
-            ems.pop();
-        }
     }
 }
